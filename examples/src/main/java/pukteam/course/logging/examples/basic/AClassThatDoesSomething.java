9cb535d015455200463cc7e085b8b1a115c654b0
aviad cohen <aviad202000@yahoo.com>
2018-04-12 23:32:23 +0300

    use file rollover appender

--- examples/src/main/java/pukteam/course/logging/examples/basic/AClassThatDoesSomething.java
+++ examples/src/main/java/pukteam/course/logging/examples/basic/AClassThatDoesSomething.java
@@ -7,13 +7,15 @@ import org.apache.logging.log4j.Logger;
 public class AClassThatDoesSomething {
 
     private static Logger logger = LogManager.getLogger(AClassThatDoesSomething.class);
-
+    private static final int TOTAL_TIMES = 1000;
     public static void foo() {
-        logger.log(Level.TRACE, "This is a TRACE message from foo");
-        logger.log(Level.DEBUG, "This is a DEBUG message from foo");
-        logger.log(Level.INFO, "This is a INFO message from foo");
-        logger.log(Level.WARN, "This is a WARN message from foo");
-        logger.log(Level.ERROR, "This is a ERROR message from foo");
-        logger.log(Level.FATAL, "This is a FATAL message from foo");
+        for (int i = 0; i < TOTAL_TIMES; i++) {
+            logger.log(Level.TRACE, "This is a TRACE message from foo # " + i);
+            logger.log(Level.DEBUG, "This is a DEBUG message from foo # " + i);
+            logger.log(Level.INFO, "This is a INFO message from foo #" + i);
+            logger.log(Level.WARN, "This is a WARN message from foo #" + i);
+            logger.log(Level.ERROR, "This is a ERROR message from foo #" + i);
+            logger.log(Level.FATAL, "This is a FATAL message from foo #" + i);
+        }
     }
 }
